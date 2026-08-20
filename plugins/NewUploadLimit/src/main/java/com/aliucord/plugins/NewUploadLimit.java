26f8e98f86fa7c4ab7f85c297e3616af9662cf34
adh01640-beep <adh01640@gmail.com>
2026-08-20 18:12:04 +0300

    Create NewUploadLimit.java

---
+++ plugins/NewUploadLimit/src/main/java/com/aliucord/plugins/NewUploadLimit.java
@@ -0,0 +1,63 @@
+package com.aliucord.plugins;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+
+import com.aliucord.entities.Plugin;
+import com.aliucord.patcher.Hook;
+
+import java.lang.reflect.Method;
+
+public class NewUploadLimit extends Plugin {
+
+    public static final long FREE_LIMIT_BYTES = 20971520L; // 20 MB
+
+    @Override
+    public void start(Context context) throws Throwable {
+        patchUploadLimitResolver();
+        patchStoreStreamFallback();
+    }
+
+    private void patchUploadLimitResolver() {
+        try {
+            Class<?> storeMediaClass = Class.forName("com.discord.stores.StoreMediaSettings");
+            patcher.patch(
+                storeMediaClass,
+                "getMaxUploadSize",
+                new Class<?>[] { boolean.class },
+                new Hook(callFrame -> {
+                    long originalLimit = (long) callFrame.getResult();
+                    if (originalLimit <= 10485760L) {
+                        callFrame.setResult(FREE_LIMIT_BYTES);
+                    }
+                })
+            );
+        } catch (Throwable ignored) {}
+    }
+
+    private void patchStoreStreamFallback() {
+        try {
+            Class<?> attachmentUtils = Class.forName("com.discord.utilities.attachments.AttachmentUtils");
+            for (Method method : attachmentUtils.getDeclaredMethods()) {
+                if (method.getReturnType().equals(boolean.class) 
+                        && method.getParameterTypes().length >= 2 
+                        && method.getParameterTypes()[0].equals(long.class)) {
+
+                    patcher.patch(method, new Hook(callFrame -> {
+                        long fileSize = (long) callFrame.args[0];
+                        long allowedLimit = (long) callFrame.args[1];
+                        long targetLimit = Math.max(allowedLimit, FREE_LIMIT_BYTES);
+                        callFrame.setResult(fileSize > targetLimit);
+                    }));
+                }
+            }
+        } catch (Throwable e) {
+            logger.error("Failed to patch upload limit check", e);
+        }
+    }
+
+    @Override
+    public void stop(Context context) {
+        patcher.unpatchAll();
+    }
+}
