7f231a1011c9a83f9f29e068b9c2abc17dd205ef
adh01640-beep <adh01640@gmail.com>
2026-08-20 18:20:09 +0300

    Update build.gradle.kts

--- plugins/NewUploadLimit/build.gradle.kts
+++ plugins/NewUploadLimit/build.gradle.kts
@@ -1,12 +1,6 @@
-plugins {
-    id("com.aliucord.gradle")
-}
-
 aliucord {
     author("Adham")
     description("Updates the free upload limit to 20MB and adapts client-side attachment checks.")
     version("1.0.0")
-    buildUrl("https://raw.githubusercontent.com/adh01640-beep/plugins/builds/NewUploadLimit.zip")
-    deploy.set(true)
+    buildUrl("https://raw.githubusercontent.com/adh01640-beep/NewUploadLimit/builds/NewUploadLimit.zip")
 }
-
